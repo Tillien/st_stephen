0be018977fe2f2a1681596918a00a82efcc0a79c
Tillien <tilliennango@gmail.com>
2021-11-22 13:21:36 +0300

    links, assets,style

--- index.php
+++ index.php
@@ -5,8 +5,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <!--google font links-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300&display=swap" rel="stylesheet">
+<!--font awesome link-->
+<script src="https://kit.fontawesome.com/e36016e380.js" crossorigin="anonymous"></script>
+<!--bootsrap link-->
+<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+<!--customs.css link-->
+<link rel="stylesheet" href="style.css">
+
 </head>
 <body>
     
+
+
+
+<script src="assets/js/jquery-3.6.0.min.js"></script>
+<script src="assets/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
