948f00e34d4ee8cddba84d52a8e6e0d15aa2e01e
Tillien <tilliennango@gmail.com>
2021-11-22 14:08:05 +0300

    header,footr phps

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!--google font links-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300&display=swap" rel="stylesheet">
+<!--font awesome link-->
+<script src="https://kit.fontawesome.com/e36016e380.js" crossorigin="anonymous"></script>
+<!--bootsrap link-->
+<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+<!--customs.css link-->
+<link rel="stylesheet" href="style.css">
+
+</head>
+<body>
